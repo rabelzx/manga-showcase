51092f50d1af1c2289619f71ebc91472d9b63867
eric <eric.rabelo21@gmail.com>
2023-04-21 17:17:56 -0300

    Página inicial finalizada

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const buttons = document.querySelectorAll(".nav-button")
+const slides = document.querySelectorAll(".video-slide")
+const content = document.querySelectorAll(".content")
 
 var sliderNav = function(manual){
     buttons.forEach((btn) => {
         btn.classList.remove("active")
+    
+    })
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    
+    })
+
+    content.forEach((cnt) => {
+        cnt.classList.remove("active")
+    
     })
     
+    content[manual].classList.add("active")
     buttons[manual].classList.add("active")
+    slides[manual].classList.add("active")
 }
 
 buttons.forEach((btn, i) => {
